7f28b3598b6849d3c4425167054e3f90ec02a16a
yervapavan <93818908+yervapavan@users.noreply.github.com>
2022-09-30 19:03:47 +0530

    Add files via upload
    level order traversal of a binary search tree

---
+++ levelordertraversal.py
@@ -0,0 +1,72 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/verticalordertraversal.py
+6 5 4 7 8 9
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python39/verticalordertraversal.py", line 42, in <module>
+    root=t.insert(root,l[0])
+  File "C:/Users/user/AppData/Local/Programs/Python/Python39/verticalordertraversal.py", line 9, in insert
+    root=Node(data)
+TypeError: Node() takes no arguments
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/verticalordertraversal.py
+6 5 4 7 8 9
+4
+5
+6
+7
+8
+9
+{0: [6], -1: [5], -2: [4], 1: [7], 2: [8], 3: [9]}
+>>> 
+=========== RESTART: C:/Users/user/Documents/verticalordertraversal.py ==========
+6 5 4 7 8 9
+4
+5
+6
+7
+8
+9
+6  
+5  
+4  
+7  
+8  
+9  
+9 8 7 4 5 6 
+>>> 
+=========== RESTART: C:/Users/user/Documents/verticalordertraversal.py ==========
+6 5 4 7 8 9
+4
+5
+6
+7
+8
+9
+6 5 4 7 8 9 9 8 7 4 5 6 
+>>> 
+=========== RESTART: C:/Users/user/Documents/verticalordertraversal.py ==========
+6 5 4 7 8 9
+4
+5
+6
+7
+8
+9
+6 5 4 7 8 9 
+9 8 7 4 5 6 
+>>> 
+==================== RESTART: C:/Users/user/Documents/Bst.py ====================
+6 5 4 7 8 9
+Traceback (most recent call last):
+  File "C:/Users/user/Documents/Bst.py", line 35, in <module>
+    t.lot(root)
+  File "C:/Users/user/Documents/Bst.py", line 18, in lot
+    if root is none:
+NameError: name 'none' is not defined
+>>> 
+==================== RESTART: C:/Users/user/Documents/Bst.py ====================
+6 5 4 7 8 9
+6 5 7 4 8 9 
+>>> 
